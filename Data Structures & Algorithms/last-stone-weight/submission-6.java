73a848d8e92f98b28c29877795829f523b08e882
OmkarDBhoir <67101370+OmkarDBhoir@users.noreply.github.com>
2026-06-02 09:42:29 +0530

    Add: last-stone-weight - submission-6

---
+++ Data Structures & Algorithms/last-stone-weight/submission-6.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> q = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        for(int i: stones) {
+            q.offer(i);
+        }
+
+        System.out.println(q);
+        while(q.size() > 1) {
+            int max1 = q.poll();
+            int max2 = q.poll();
+            if(max1 > max2) {
+                q.offer(max1 - max2);
+            }
+        }
+
+        return q.isEmpty() ? 0 : q.peek();
+    }
+}

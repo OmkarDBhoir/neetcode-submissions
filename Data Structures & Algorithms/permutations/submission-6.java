26168d7854ecbca6e3054bcc71d8bccc475a53ca
OmkarDBhoir <67101370+OmkarDBhoir@users.noreply.github.com>
2026-06-28 01:34:54 +0530

    Add: permutations - submission-6

---
+++ Data Structures & Algorithms/permutations/submission-6.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        return permute(nums, 0);
+    }
+
+    public List<List<Integer>> permute(int[] nums, int index) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(index == nums.length) {
+            List<Integer> lst = new ArrayList<>();
+            for(int i: nums) {
+                lst.add(i);
+            }
+            ans.add(lst);
+            return ans;
+        }
+        for(int i=index; i<nums.length; i++) {
+            swap(nums, index, i);
+            ans.addAll(permute(nums, index+1));
+            swap(nums, index, i);
+        }
+        return ans;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
